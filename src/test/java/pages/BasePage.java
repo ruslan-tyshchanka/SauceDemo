1ac022b861ee329243af49307c8d553e1bb9d38f
Ruslan Tyshchanka <ruslantyshchenko99@gmail.com>
2023-12-13 20:19:02 +0300

    Update int to Duration

--- src/test/java/pages/BasePage.java
+++ src/test/java/pages/BasePage.java
@@ -14,6 +14,6 @@ public abstract class BasePage {
 
     BasePage(WebDriver driver) {
         this.driver = driver;
-        wait = new WebDriverWait(driver, 20);
+        wait = new WebDriverWait(driver, Duration.ofSeconds(20));
     }
 }
